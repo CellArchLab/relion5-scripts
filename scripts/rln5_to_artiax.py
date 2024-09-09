28aa7370d07c6bf3e91bbf5a2a9607d08f2e774e
Ricardo Righetto <ricardorighetto@gmail.com>
2024-09-09 11:53:25 +0200

    New scripts

---
+++ scripts/rln5_to_artiax.py
@@ -0,0 +1,48 @@
+#!/scicore/projects/scicore-p-structsoft/ubuntu/software/Miniconda3/miniconda3_python3.12.1/envs/relion-5.0/bin/python
+
+import argparse
+import starfile
+import sys
+import numpy as np
+
+def parse_args():
+		
+	parser = argparse.ArgumentParser(description="Make RELION-5 STAR files compatible with ArtiaX.")
+	parser.add_argument("--instar", type=str, help="Path to input RELION-5 STAR file.")
+	parser.add_argument("--outstar", type=str, help="Path to output RELION-5 STAR file.")
+	parser.add_argument("--x", type=float, help="X dimension of tomogram for visualization.")
+	parser.add_argument("--y", type=float, help="Y dimension of tomogram for visualization.")
+	parser.add_argument("--z", type=float, help="Z dimension of tomogram for visualization.")
+		
+	if len(sys.argv) == 1:
+		parser.print_help(sys.stderr)
+		sys.exit(1)
+
+	return parser.parse_args()
+
+def main():
+
+	args = parse_args()
+
+	instar = args.instar
+	outstar = args.outstar
+	x = args.x
+	y = args.y
+	z = args.z
+
+	rln = starfile.read( instar )
+	rln['particles']['rlnTomoNameOriginal'] = ""
+	for idx,tomo in enumerate( np.unique( rln['particles']['rlnTomoName'] ) ):
+		
+		sel = rln['particles']['rlnTomoName'] == tomo
+		rln['particles'].loc[sel, 'rlnTomoNameOriginal'] = tomo
+		rln['particles'].loc[sel, 'rlnTomoName'] = f"tomo_{idx+1:04}"
+
+	rln['particles']['rlnCoordinateX'] = rln['particles']['rlnCenteredCoordinateXAngst'] / rln['particles']['rlnDetectorPixelSize'] + x / 2
+	rln['particles']['rlnCoordinateY'] = rln['particles']['rlnCenteredCoordinateYAngst'] / rln['particles']['rlnDetectorPixelSize'] + y / 2
+	rln['particles']['rlnCoordinateZ'] = rln['particles']['rlnCenteredCoordinateZAngst'] / rln['particles']['rlnDetectorPixelSize'] + z / 2
+
+	starfile.write( rln, outstar, overwrite=True) 
+		
+if __name__ == "__main__":
+	main()
---
+++ scripts/rln5_to_imod.py
@@ -0,0 +1,62 @@
+#!/scicore/projects/scicore-p-structsoft/ubuntu/software/Miniconda3/miniconda3_python3.12.1/envs/relion-5.0/bin/python
+
+import argparse
+import starfile
+import sys
+import os
+import numpy as np
+import subprocess
+
+def parse_args():
+		
+	parser = argparse.ArgumentParser(description="Randomize half-sets based on odd/even order of particles.")
+	parser.add_argument("--instar", type=str, help="Path to input RELION-5 STAR file.")
+	parser.add_argument("--outmod", type=str, help="Path to output IMOD model file.")
+	parser.add_argument("--tomo", type=str, help="Name of tomogram for visualization.")
+	parser.add_argument("--angpix", type=float, help="Pixel size of the tomogram for visualization.")
+	parser.add_argument("--x", type=float, help="X dimension of tomogram for visualization.")
+	parser.add_argument("--y", type=float, help="Y dimension of tomogram for visualization.")
+	parser.add_argument("--z", type=float, help="Z dimension of tomogram for visualization.")
+		
+	if len(sys.argv) == 1:
+		parser.print_help(sys.stderr)
+		sys.exit(1)
+
+	return parser.parse_args()
+
+def main():
+
+	args = parse_args()
+
+	instar = args.instar
+	outmod = args.outmod
+	outtxt = os.path.splitext(outmod)[0] + '.txt'
+	tomo = args.tomo
+	x = args.x
+	y = args.y
+	z = args.z
+	apix = args.angpix
+
+	rln = starfile.read( instar )
+		
+	sel = rln['particles']['rlnTomoName'] == tomo
+
+	xyz_centered_ang = np.array( [ rln['particles'].loc[sel, 'rlnCenteredCoordinateXAngst'], rln['particles'].loc[sel, 'rlnCenteredCoordinateYAngst'], rln['particles'].loc[sel, 'rlnCenteredCoordinateZAngst'] ] )
+	# print(xyz_centered_ang.shape)
+
+	if 'rlnOriginZAngst' in rln['particles'].keys():
+		xyz_centered_ang -= np.array( [ rln['particles'].loc[sel, 'rlnOriginXAngst'], rln['particles'].loc[sel, 'rlnOriginYAngst'], rln['particles'].loc[sel, 'rlnOriginZAngst'] ] )
+
+	xyz_centered = np.transpose(xyz_centered_ang) / apix
+	xyz = xyz_centered
+	xyz[:,0] += x / 2
+	xyz[:,1] += y / 2
+	xyz[:,2] += z / 2
+
+	# Save the array to a text file (e.g., 'output.txt')
+	np.savetxt(outtxt, xyz, fmt='%.2f', delimiter='  ')
+	command = "point2model -scat -sphere 10 -pixel %.3f,%.3f,%.3f %s %s" % (apix, apix, apix, outtxt, outmod) 
+	subprocess.run(command, shell = True, executable="/bin/bash")
+		
+if __name__ == "__main__":
+	main()
